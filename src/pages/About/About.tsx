466cb75056ed9089f3a7c79f4c9a02c6dc9220eb
Tehzudteen <luckyteh0083@gmail.com>
2025-02-12 17:16:40 +0700

    edit2

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About: React.FC = () => {
   return (
-    <div className="w-full h-screen my-10">
+    <div className="w-full my-10 ">
       <iframe
         src="https://my.spline.design/draganddropbookpencilschoolcopy-9947f7d3d8bf786883d7913d954b3fe8/"
         width="100%"
