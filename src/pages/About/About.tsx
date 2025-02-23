4d7b9cab396c3a2df67fbf9ab883d03a897f448e
Tehzudteen <luckyteh0083@gmail.com>
2025-02-24 03:18:58 +0700

    d

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -8,8 +8,8 @@ import { Link } from "react-router-dom"; // import สำหรับการ�
 
 const About: FC = () => {
   return (
-    <div className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
-      <Logo />
+    <div className="min-h-screen my-10 bg-zinc-900 px-4 py-12 text-zinc-50">
+    
       <motion.div
         initial="initial"
         animate="animate"
@@ -315,24 +315,7 @@ const LinkItem = ({ heading, imgSrc, subheading, href }: LinkItemProps) => {
   );
 };
 
-const Logo = () => (
-  <svg
-    width="40"
-    height="auto"
-    viewBox="0 0 50 39"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="mx-auto mb-12 fill-zinc-50"
-  >
-    <path
-      d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
-      stopColor="#000000"
-    ></path>
-    <path
-      d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
-      stopColor="#000000"
-    ></path>
-  </svg>
-);
+
+
 
 export default About;
